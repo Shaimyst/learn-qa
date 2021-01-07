174cbed15acd457205be23d878855ce517296786
Jessica Sadler <jessicagsadler@gmail.com>
2021-01-07 21:46:46 +0100

    add new selenium automation project with google search test

---
+++ selenium-automation/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace selenium_automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a reference for Chrome browser
+            IWebDriver driver = new ChromeDriver(); 
+
+            // Go to Google Page
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            //Make the browser fullscreen
+            driver.Manage().Window.Maximize();
+
+            // Find the element
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+
+            //Type something in the search bar
+            searchBox.SendKeys("google news");
+
+            //Close the browser
+            driver.Quit();
+        }
+    }
+}
